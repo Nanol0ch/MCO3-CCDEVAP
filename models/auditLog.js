8be45c04a21de8322b9e6c809971a4a793ddb249
Georgina Ravelo <96278205+georgeravlo@users.noreply.github.com>
2026-08-05 20:52:05 +0800

    Add audit log schema for mongoose

--- models/auditLog.js
+++ models/auditLog.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const auditLogSchema = new mongoose.Schema({
+    timestamp: { type: Date, default: Date.now },
+    username: { type: String, required: true },
+    role: { type: String, required: true },
+    activity: { type: String, required: true }
+});
+
+module.exports = mongoose.model('AuditLog', auditLogSchema);
