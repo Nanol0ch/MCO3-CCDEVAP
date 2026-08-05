0b12ec787300f1e60956707f969e4d51d51bbe8e
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-08-05 10:29:08 +0800

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
+
+router.get('/register', authController.getRegisterPage);
+router.post('/register', authController.registerUser);
+
+router.get('/login', authController.getLoginPage);
+router.post('/login', authController.loginUser);
+
+router.get('/logout', authController.logoutUser);
+
+router.get('/', userController.getHomePage);
+router.get('/profile', userController.getProfile);
+router.post('/profile', userController.updateProfile);
+
+module.exports = router;
 
