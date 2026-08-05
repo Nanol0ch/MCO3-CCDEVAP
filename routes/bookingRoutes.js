ce9b3c0b9835233d8f3b8a80762c7d631a6e2a6b
saladsisar <caesarg965@gmail.com>
2026-08-05 20:26:29 +0800

    added line 4 and adjusted 6,7,8

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
+const { isAuthenticated } = require('../middleware/authMiddleware');
 
-router.get('/:flightId', bookingController.getBookingPage);
-router.get('/api/flights/:id/occupied-seats', bookingController.getOccupiedSeats);
-router.post('/', bookingController.submitBooking);
+router.get('/:flightId', isAuthenticated, bookingController.getBookingPage);
+router.get('/api/flights/:id/occupied-seats',isAuthenticated, bookingController.getOccupiedSeats);
+router.post('/', isAuthenticated, bookingController.submitBooking);
 
 module.exports = router;
 
