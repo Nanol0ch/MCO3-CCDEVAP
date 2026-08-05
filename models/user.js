be6fe0d281aaf0866053862922b9c7816dbfa222
saladsisar <caesarg965@gmail.com>
2026-08-05 20:20:52 +0800

    added role

--- models/user.js
+++ models/user.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    role: {
+        type: String,
+        enum: ['admin', 'passenger'],
+        default: 'passenger'
+    },
     passportNumber: {
         type: String,
         required: true
