44f41a679fc248effdf0ffe21c664af459f6b441
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-08-05 10:26:07 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    passportNumber: {
+        type: String,
+        required: true
+    },
+    nationality: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
+
 
