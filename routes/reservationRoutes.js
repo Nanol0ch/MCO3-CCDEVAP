32de26d4f68b4a38a20b3876fcd7513c02a19812
saladsisar <caesarg965@gmail.com>
2026-08-05 20:27:42 +0800

    added line 4 and adjusted 6,7,8

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const reservationController = require('../controllers/reservationController');
+const { isAuthenticated } = require('../middleware/authMiddleware');
 
-router.get('/', reservationController.getMyReservations);
-router.patch('/:id/seat', reservationController.updateSeat);
-router.delete('/:id', reservationController.cancelReservation);
+router.get('/', isAuthenticated, reservationController.getMyReservations);
+router.patch('/:id/seat', isAuthenticated, reservationController.updateSeat);
+router.delete('/:id', isAuthenticated, reservationController.cancelReservation);
 
 module.exports = router;
 
