49f7db02a38336d0e2917d74111ef7607f4dcacf
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-08-05 10:29:23 +0800

    Add booking routes for flight bookings

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const bookingController = require('../controllers/bookingController');
+
+router.get('/:flightId', bookingController.getBookingPage);
+router.get('/api/flights/:id/occupied-seats', bookingController.getOccupiedSeats);
+router.post('/', bookingController.submitBooking);
+
+module.exports = router;
 
