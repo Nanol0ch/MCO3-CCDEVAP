6bf0a7c8306b472944e9818ff2dba367aa1d25f0
saladsisar <caesarg965@gmail.com>
2026-08-05 20:18:15 +0800

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,11 @@
+const isAuthenticated = (req, res, next) => {
+    if (req.session && req.session.user) {
+        return next();
+    }
 
+    return res.redirect('/login');
+};
+
+module.exports = {
+    isAuthenticated
+};
