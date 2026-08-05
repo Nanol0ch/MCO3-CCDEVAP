0457693373f34ae9391779449d1397a39ecfbe66
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-08-05 10:28:44 +0800

    Add admin routes for flights and reservations

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/adminController');
+
+router.get('/', adminController.getDashboard);
+router.get('/users', adminController.getUsersPage);
+
+router.get('/flights', adminController.getFlights);
+router.post('/flights', adminController.createFlight);
+router.put('/flights/:id', adminController.updateFlight);
+router.delete('/flights/:id', adminController.deleteFlight);
+
+router.get('/reservations', adminController.getAllReservations);
+router.patch('/reservations/:id/status', adminController.updateReservationStatus);
+
+module.exports = router;
 
