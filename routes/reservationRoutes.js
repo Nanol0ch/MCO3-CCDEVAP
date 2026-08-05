628259c31029f931f8fa79bbb42e3b65b019f989
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-08-05 10:29:58 +0800

    Update reservationRoutes.js

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const reservationController = require('../controllers/reservationController');
+
+router.get('/', reservationController.getMyReservations);
+router.patch('/:id/seat', reservationController.updateSeat);
+router.delete('/:id', reservationController.cancelReservation);
+
+module.exports = router;
 
