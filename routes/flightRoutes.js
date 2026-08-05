b81da609fb4db10d25c24d6cdb93ed5b043473ba
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-08-05 10:29:39 +0800

    Add flight routes for searching and retrieving flights

--- routes/flightRoutes.js
+++ routes/flightRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const flightController = require('../controllers/flightController');
+
+router.get('/search', flightController.getSearchPage);
+router.get('/search/results', flightController.searchFlights);
+router.get('/:id', flightController.getFlightById);
+
+module.exports = router;
 
