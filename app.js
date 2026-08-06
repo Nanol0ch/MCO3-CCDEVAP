d73f988f2a35dde414a88eb8cfa2f406716764f6
Nanol0ch <aaustriacarlos8@gmail.com>
2026-08-06 15:30:18 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use('/bookings', require('./routes/bookingRoutes'));
 app.use('/reservations', require('./routes/reservationRoutes'));
 app.use('/admin', require('./routes/adminRoutes'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 80;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
