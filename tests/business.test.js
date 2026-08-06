0cb5a2217a07cf3520a974d8f1eff0f9cee83dea
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-08-06 17:08:37 +0800

    Update business.test.js

--- tests/business.test.js
+++ tests/business.test.js
@@ -3,7 +3,8 @@ const Flight = require('../models/flight');
 const Reservation = require('../models/reservation');
 
 beforeAll(async () => {
-    await mongoose.connect('mongodb://localhost:27017/airroute_test');
+    const testUri = (process.env.MONGO_URI || 'mongodb://localhost:27017/airroute').replace('/airroute', '/airroute_test');
+    await mongoose.connect(testUri);
 });
 
 afterAll(async () => {
